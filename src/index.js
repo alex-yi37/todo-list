8b5946f8e8f2eb97e7bc8b07579000d5facf61d4
alex <alexanderyi933@gmail.com>
2023-03-09 15:31:38 -0500

    append a new todo dom element on form submit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 function main() {
-  let todoList = [];
   document
     .querySelector("#create-todo-form")
     .addEventListener("submit", function (event) {
       event.preventDefault();
+      
       let title = document.querySelector("#todo-title-input").value;
       let description = document.querySelector("#todo-description-input").value;
-      todoList.push({ title, description });
+
       document.querySelector("#todo-title-input").value = "";
       document.querySelector("#todo-description-input").value = "";
+
+      appendTodo(title, description)
     });
 }
 
@@ -16,4 +18,17 @@ function displayTitleDescriptionInfo(response) {
     
 }
 
+function appendTodo(title, description) {
+  // 1. create a new li tag
+  const newTodoElement = document.createElement("li")
+  // 2. set the text inside
+  newTodoElement.textContent = `Title: ${title} - Desc: ${desc}`
+
+  // 3. get the ul element that has all the todo li elements
+  const ulContainer = document.querySelector("#todo-container")
+  // 4. append the new li tag to the ul
+  ulContainer.appendChild(newTodoElement)
+}
+
 main()
+
