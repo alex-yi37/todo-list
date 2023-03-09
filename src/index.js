363e3a757cb6cb8d773114c1242fd3d0277028b8
Namix23 <98861500+Namix23@users.noreply.github.com>
2023-03-09 16:03:22 -0500

    Create todo elements to display title, description inputs

--- src/index.js
+++ src/index.js
@@ -3,32 +3,38 @@ function main() {
     .querySelector("#create-todo-form")
     .addEventListener("submit", function (event) {
       event.preventDefault();
-      
+
       let title = document.querySelector("#todo-title-input").value;
       let description = document.querySelector("#todo-description-input").value;
 
       document.querySelector("#todo-title-input").value = "";
       document.querySelector("#todo-description-input").value = "";
 
-      appendTodo(title, description)
+      appendTodo(title, description);
     });
 }
 
-function displayTitleDescriptionInfo(response) {
-    
-}
+function displayTitleDescriptionInfo(response) {}
 
 function appendTodo(title, description) {
   // 1. create a new li tag
-  const newTodoElement = document.createElement("li")
+
+  const newTodoElement = document.createElement("li");
   // 2. set the text inside
-  newTodoElement.textContent = `Title: ${title} - Desc: ${desc}`
+
+  let titleTodo = document.createElement("h3");
+  titleTodo.textContent = `${title}`;
+  newTodoElement.appendChild(titleTodo);
+
+  let descriptionTodo = document.createElement("p");
+  descriptionTodo.textContent = `${description}`;
+  newTodoElement.appendChild(descriptionTodo);
 
   // 3. get the ul element that has all the todo li elements
-  const ulContainer = document.querySelector("#todo-container")
+  const ulContainer = document.querySelector("#todo-container");
+
   // 4. append the new li tag to the ul
-  ulContainer.appendChild(newTodoElement)
+  ulContainer.appendChild(newTodoElement);
 }
 
-main()
-
+main();
