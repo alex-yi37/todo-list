2b317dad0bd18d261130f3ce7276636c3315e92c
Namix23 <98861500+Namix23@users.noreply.github.com>
2023-03-17 04:17:26 -0400

    Add js comments, ability for delete button to be placed bottom of title/description

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-function main() {
+//select form and label w/o webpage refreshing, returning their input values to be inserted
+function main(response) {
   document
     .querySelector("#create-todo-form")
     .addEventListener("submit", function (event) {
       event.preventDefault();
 
-      let title = document.querySelector("#todo-title-input").value;
-      let description = document.querySelector("#todo-description-input").value;
+      const title = document.querySelector("#todo-title-input").value;
+      const description = document.querySelector(
+        "#todo-description-input"
+      ).value;
 
       document.querySelector("#todo-title-input").value = "";
       document.querySelector("#todo-description-input").value = "";
@@ -14,42 +17,38 @@ function main() {
     });
 }
 
-function displayTitleDescriptionInfo(response) {}
-
+// select ul, create a new li, div and button tag
 function appendTodo(title, description) {
-  // create a new div, li, ul tag
-
-  const titleContainer = document.createElement("div");
-
-  let todoListContainer = document.querySelector("#todo-container"); //parent
-
+  const todoListContainer = document.querySelector("#todo-container"); //parent
   const newTodoElement = document.createElement("li"); //child
-
-  // set the text inside
-
+  const titleContainer = document.createElement("div");
   const deleteButton = document.createElement("button");
 
-  //delete li
-
+  // remove contents inside li tag
   deleteButton.addEventListener("click", function () {
     todoListContainer.removeChild(newTodoElement);
   });
 
-  let titleTodo = document.createElement("h3");
-  titleTodo.textContent = `${title}`;
-  titleContainer.appendChild(titleTodo);
-  titleContainer.appendChild(deleteButton);
-  newTodoElement.appendChild(titleContainer);
-
-  let descriptionTodo = document.createElement("p");
-  descriptionTodo.textContent = `${description}`;
-  newTodoElement.appendChild(descriptionTodo);
-  deleteButton.textContent = "delete";
-
-  // 3. get the ul element that has all the todo li elements
+  //create a new h3 and p tag, select ul tag
+  const titleTodo = document.createElement("h3");
+  const descriptionTodo = document.createElement("p");
   const ulContainer = document.querySelector("#todo-container");
 
-  // 4. append the new li tag to the ul
+  //Add content to h3 and p tag
+  titleTodo.textContent = `${title}`;
+  descriptionTodo.textContent = `${description}`;
+
+  newTodoElement.appendChild(titleContainer); // append div tag to li
+  titleContainer.appendChild(titleTodo); // append h3 tag to div
+  titleContainer.appendChild(descriptionTodo); // append p tag to div
+  titleContainer.appendChild(deleteButton); // append button tag to div
+
+  // set text inside button
+  deleteButton.textContent = "delete";
+
+  // 3. select ul element that has all the todo li elements
+
+  // append the new li tag to the ul
   ulContainer.appendChild(newTodoElement);
 }
 
